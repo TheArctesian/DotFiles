cf67715adf8930fb9763fb23630df3a6b459bd29
theArctesian <stephen.d.okita@gmail.com>
2022-01-12 17:02:32 +0800

    ubuntu works

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,30 +1,30 @@
-
+# chmod +x ubuntu.sh 
 # System update
 sudo apt update -y
-sudo apt upgrade -y
+sudo apt upgrade -ybefore 
 sudo apt install snapd
 
 # Install packages
 
 
-sudo apt install curl
-sudo apt install exfat-utils
-sudo apt install file
-sudo apt install git
-sudo apt install bpytop
-sudo apt install cointop
-sudo apt install tmux
-sudo apt install vim
+sudo apt install curl -y
+sudo apt install exfat-utils -y
+sudo apt install file -y
+sudo apt install git -y
+sudo apt install vim -y
 
 # Creative
-sudo apt install gimp
-sudo snap install blender
+sudo apt install gimp -y
+sudo snap install blender -y
 
 # Development
-sudo snap install code --classic
-sudo apt install build-essential
-sudo apt install python3
-sudo apt install python3-pip
+sudo snap install code --classic -y
+sudo apt install build-essential -y
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+sudo snap install bpytop 
+sudo snap install cointop
+sudo snap install tmux
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # Rust
 
 # RW
@@ -35,33 +35,35 @@ sudo snap install signal-desktop
 sudo snap install discord
 
 # Fun stuff
-sudo apt install figlet
-sudo apt install lolcat
+sudo apt install figlet -y
+sudo apt install lolcat -y
 
 
 # npm
-sudo apt install nodejs
-sudo npm install -g sass sass-lint
-sudo npm install -g typescript
-sudo npm install -g prettier
-sudo npm install -g yarn
-sudo npm install -g truffle
+sudo apt install nodejs -y
+sudo npm install -g sass sass-lint -y
+sudo npm install -g typescript -y
+sudo npm install -g prettier -y
+sudo npm install -g yarn -y
+sudo npm install -g truffle -y
 
 # rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo apt install rustc
-sudo apt install cargo
-
+sudo apt install rustc 
+sudo apt install cargo 
 
 # fish 
-sudo apt install fish
-sudo apt install fonts-firacode
-sudo apt install fonts-hack-ttf
+sudo apt install fish -y
+sudo apt install fonts-firacode -y
+sudo apt install fonts-hack-ttf -y
 curl -L https://get.oh-my.fish | fish
 chsh -s `which fish`
 
 # Make dir 
+cd
 cd desktop
 mkdir -p ~/Desktop/{scripts, school, work}
 cd school 
 git clone https://github.com/TheArctesian/SchoolFiles.git
+git config --global user.email "stephen.d.okita@gmail.com"
+git config --global user.name "theArctesian"
