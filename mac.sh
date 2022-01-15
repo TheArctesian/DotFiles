b9b8bdfe919ec84772e7367973303e704c6bffe3
TheArctesian <stephen.d.okita@gmail.com>
2022-01-15 18:19:18 +0800

    mac.sh

---
+++ mac.sh
@@ -0,0 +1,29 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git
+brew install node
+brew install yarn
+brew install wget
+brew install fish
+sudo echo /usr/local/bin/fish >> /etc/shells
+chsh -s /usr/local/bin/fish
+fish
+set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
+brew install tmux
+brew install curl
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+omf install slavic-cat
+brew install python3
+brew install python3-pip
+pip3 install --upgrade pip
+brew install vim
+brew install bpytop
+brew install go
+brew install openjdk
+brew install --cask visual-studio-code
+brew install --cask spotify
+brew install --cask zoom
+brew install --cask discord
+brew install --cask brave-browser
+brew install --cask signal
+brew install --cask libreoffice
+brew install --cask tor-browser
