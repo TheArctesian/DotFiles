0ff2fd0686dd5647870a77bb0f8e73ba845378be
TheArctesian <stephen.d.okita@gmail.com>
2022-03-23 12:39:03 +0800

    pretty stuff that dosn't function

--- Übersicht/pecan/workspace.jsx
+++
@@ -1,9 +0,0 @@
-const command = 'bash pecan/scripts/ws'
-const refreshFrequency = 1000 // ms
-const render = ({ output }) => (
-  <div class="screen">
-    <div class="pecanworkspace">{`${output}`}</div>
-  </div>
-)
-
-export { command, refreshFrequency, render }
