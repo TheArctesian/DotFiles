c4ac6c0b4127a8f95e4864eed9d00b55d1f70eb6
theArctesian <stephen.d.okita@gmail.com>
2022-03-25 16:51:36 +0800

    miner

---
+++ Miner/mine_eth.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+POOL=asia2.ethermine.org:4444 
+WALLET=0x206BcCF64aB61A0Bd90F97732625815aDe3AbD33.bigboi
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cd "$(dirname "$0")"
+
+./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
+while [ $? -eq 42 ]; do
+    sleep 10s
+    ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
+done
