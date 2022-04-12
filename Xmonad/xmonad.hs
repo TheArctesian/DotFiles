ac0358655311fc46d7d5c0980ed5b4705514ede7
theArctesian <stephen.d.okita@gmail.com>
2022-04-13 05:33:15 +0800

    updates

--- Xmonad/xmonad.hs
+++ Xmonad/xmonad.hs
@@ -49,19 +49,32 @@ import XMonad.Prompt.Window
 import XMonad.Prompt.ConfirmPrompt
 import XMonad.Prompt.Shell
 import XMonad.Prompt.FuzzyMatch
+
+-- Colors
+import Colors.Dracula
 --------------------------------------------------------------------------------------------------------------------------------------------
 -- User Vars
 myModMask              = mod4Mask                                                                 :: KeyMask
 myFocusFollowsMouse    = True                                                                     :: Bool
 myClickJustFocuses     = False                                                                    :: Bool
 myBorderWidth          = 2                                                                        :: Dimension
-myWindowGap            = 20                                                                       :: Integer
+myWindowGap            = 25                                                                       :: Integer
 myTerminal             = "alacritty"                                                              :: String
-myFocusedBorderColor   = "#9ec0ff"                                                                :: String
+myFocusedBorderColor   = "#bd93f9"                                                                :: String
 myNormalBorderColor    = "#000000"                                                                :: String
 myBrowser              = "brave"                                                                  :: String
+-- myWorkspaces = [" 一 ", " 二 "," 三 ", " 四", " 五 ", " 六", " 七", " 八 ", " 九 "]
+myWorkspaces = [" 1 ", " 2 "," 3 ", " 4 ", " 5 ", " 6", " 7", " 8 ", " 9 "]
+
+windowCount :: X (Maybe String)
+windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
+
 --------------------------------------------------------------------------------------------------------------------------------------------
 -- Layout
+myWorkspaceIndices = M.fromList $ zipWith (,) myWorkspaces [1..] -- (,) == \x y -> (x,y)
+
+clickable ws = "<action=xdotool key super+"++show i++">"++ws++"</action>"
+    where i = fromJust $ M.lookup ws myWorkspaceIndices
 mySpacing :: Integer -> l a -> ModifiedLayout Spacing l a
 mySpacing i = spacingRaw False (Border 0 i 0 i) True (Border i 0 i 0) True
 
@@ -102,9 +115,9 @@ myLayout =
 
 main :: IO ()
 main = do
-  xmproc <- spawnPipe "~/.fehbg"
-  xmproc <- spawnPipe "picom"
-  xmproc <- spawnPipe "xmobar"
+  xmproc0 <- spawnPipe "~/.fehbg"
+  xmproc1 <- spawnPipe "picom"
+  xmproc2 <- spawnPipe "xmobar"
   xmonad $ docks $ ewmh def
         { terminal           = myTerminal,
           focusFollowsMouse  = myFocusFollowsMouse,
@@ -113,5 +126,30 @@ main = do
           modMask            = myModMask,
           focusedBorderColor = myFocusedBorderColor,
           layoutHook         = myLayout,
-          normalBorderColor  = myNormalBorderColor
+          normalBorderColor  = myNormalBorderColor,
+          workspaces         = myWorkspaces,
+          logHook            = dynamicLogWithPP $ namedScratchpadFilterOutWorkspacePP $ xmobarPP {
+                ppOutput = \x -> hPutStrLn xmproc2 x   -- xmobar on monitor 1
+-- Current workspace
+              , ppCurrent = xmobarColor color06 "" . wrap
+                            ("<box type=Bottom width=2 mb=2 color=" ++ color06 ++ ">") "</box>"
+                -- Visible but not current workspace
+              , ppVisible = xmobarColor color06 "" . clickable
+                -- Hidden workspace
+              , ppHidden = xmobarColor color05 "" . wrap
+                           ("<box type=Top width=2 mt=2 color=" ++ color05 ++ ">") "</box>" . clickable
+                -- Hidden workspaces (no windows)
+              , ppHiddenNoWindows = xmobarColor color05 ""  . clickable
+                -- Title of active window
+              , ppTitle = xmobarColor color16 "" . shorten 60
+                -- Separator character
+              , ppSep =  "<fc=" ++ color09 ++ "> <fn=1>|</fn> </fc>"
+                -- Urgent workspace
+              , ppUrgent = xmobarColor color02 "" . wrap "!" "!"
+                -- Adding # of windows on current workspace to the bar
+              , ppExtras  = [windowCount]
+                -- order of things in xmobar
+              , ppOrder  = \(ws:l:t:ex) -> [ws,l]++ex++[t]
+
         }
+}
