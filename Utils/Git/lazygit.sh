801999567bf5db73a7e8c3c93c2ba7c79840cbc2
theArctesian <stephen.d.okita@gmail.com>
2022-04-18 18:10:31 +0800

    update lazy git

--- Utils/Git/lazygit.sh
+++ Utils/Git/lazygit.sh
@@ -4,3 +4,9 @@ read commitMessage
 git add .
 git commit -m "$commitMessage"
 git push
+
+# you should copy this to your bin so it is a exacutable
+# to do this first make sure it is a executable
+# chmod +x lazygit.sh
+# then copy it to your bin directory
+# sudo cp lazygit.sh ~/bin/
