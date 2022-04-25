4127a4fd23b32c9c8bef149e3d34b46180caf035
theArctesian <stephen.d.okita@gmail.com>
2022-04-26 07:36:52 +0800

    fish

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,7 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
 alias ls "exa -l"
 alias vim "nvim"
-# Created by `pipx` on 2022-02-16 13:59:26
-set PATH $PATH /home/daniel/.local/bin
+alias p "ping -c 3 gentoo.org"
+alias clock "tty-clock -c -C 4 -S"
