cb8e556c61f3757b6725e1f71742455f7f78a409
TheArctesian <stephen.d.okita@gmail.com>
2022-05-06 22:05:52 +0800

    reorg

---
+++ installScripts/Arch/core.sh
@@ -0,0 +1,99 @@
+timedatectl set-ntp true
+fdisk /dev/nvme0n1
+
+# File System
+mkfs.fat -F 32 /dev/nvme0n1p1
+mkswap /dev/nvme0n1p2
+mkfs.ext4 /dev/nvme0n1p3
+
+# Pacman Repo Setup
+pacman -Syy
+pacman -S reflector
+reflector -c "HK" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
+
+# Mount 
+mkdir /mnt/boot
+mount /dev/nvme0n1p3 /mnt
+mount /dev/nvme01np1 /mnt/boot
+swapon /dev/nvme0n1p2
+
+# Make Arch System
+pacstrap /mnt base linux linux-firmware vim base-devel
+
+# Gen fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Change root
+arch-chroot /mnt
+
+# Set Hardware Clock 
+ln -sf /usr/share/zoneinfo/Asia/Hong_Kong /etc/localtime
+hwclock --systohc
+
+# Locale
+vim /etc/locale.gen
+locale-gen
+
+# Hostname
+vim /etc/hostname 
+#set hostname
+# the 
+# or arctesian or daniel
+
+# Hosts 
+vim /etc/hosts
+# 127.0.0.1 localhost
+# ::1 localhost
+# 127.0.1.1 the
+
+# Set root password 
+passwd
+
+# Bootloader (Grub) TODO: Try pure pure sysd boot of lillo 
+pacman -S grub efibootmgr
+
+# Make boot efi dir
+mkdir /boot/efi
+mount /dev/nvme0n1p1 /boot/efi
+
+# Grub install
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Make User
+useradd -m -G users,wheel,audio -s /bin/bash arctesian
+passwd arctesian
+
+# Sudo cuz yay dosnt support doas 
+pacman -S sudo vi
+visudo
+# uncoment the wheel group 
+# or directly add the user
+# arctesian ALL=(ALL) ALL
+
+# or 
+pacman -S doas 
+# Install Cinnamon 
+pacman -S xorg xterm cinnamon alacritty gdm
+
+# Install Xmonad 
+pacman -S xmonad xmonad-contrib xmobar xmonad-utils feh picom ttf-fira-mono
+
+# Switch user now 
+su arctesian
+
+# Plug
+sudo pacman -S neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Xmonad stuff
+mkdir ~/.xmonad
+git clone https://github.com/TheArctesian/DotFiles.git
+cp -r ~/DotFiles/xmobar ~/.config 
+cp ~/DotFiles/xmonad/xmonad.hs ~/.config 
+cp -r ~/DotFiles/Wallpaper ~/ 
+# feh --bg-fill ~/Wallpaper/1080/whatever you want.png
+cp ~/DotFiles/Xmonad/xmonad.hs ~/.xmonad 
+
+systemctl enable sddm.service
+systemctl enable NetworkManager.service
--- installScripts/Gentoo/gentoo.sh
+++ installScripts/Gentoo/gentoo.sh
@@ -29,15 +29,15 @@ VIDEO_CARDS="nvidia"
 ## (For AMD/ATI cards)
 VIDEO_CARDS="radeon amdgpu"
 
-
+# Config mirrors
 mirrorselect -i -o >> /mnt/gentoo/etc/portage/make.conf
 
+# Config Repos 
 mkdir --parents /mnt/gentoo/etc/portage/repos.conf
-
 cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
-
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 
+# Mount system 
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
@@ -46,123 +46,118 @@ mount --make-rslave /mnt/gentoo/dev
 mount --bind /run /mnt/gentoo/run
 mount --make-slave /mnt/gentoo/run
 
+# Choroot to /mnt/gentoo 
 chroot /mnt/gentoo /bin/bash
 source /etc/profile
 export PS1="(choo choo) ${PS1}"
 
+# Mount boot system partition
 mount /dev/nvme0n1p1 /boot
 
-emerge-webrsync
-
+# Update Portage 
+emerge-webrsync 
 emerge --sync -q 
 
+# Select profile
 eselect profile list
-
 eselect profile set x
 
+# Emerge worldset 
 emerge --ask --verbose --update --deep --newuse @world
 
-echo "Asia/Hong_Kong" > /etc/timezone
+# Set timezone 
+echo "Asia/Hong_Kong" > /etc/timezone 
 
+# Set locale
 emerge --config sys-libs/timezone-data
-
 vim /etc/locale.gen
-
-# add the right things 
 locale-gen
-
 eselect locale list
-
 eselect locale set x
 
+# Rechroot
 env-update && source /etc/profile && export PS1="(choo choo) ${PS1}"
 
+# Kernel Time
 emerge --ask sys-kernel/linux-firmware
-
 emerge --ask sys-kernel/gentoo-sources
-
 eselect kernel list
-
 eselect kernel set 1
-
 emerge --ask sys-apps/pciutils
-
 emerge --ask sys-kernel/installkernel-gentoo
-
 emerge --ask sys-kernel/gentoo-kernel
-
 emerge --depclean
-
 emerge --ask @module-rebuild
 
+# Fstab 
 vim /etc/fstab
 
+# Hostname
 vim /etc/conf.d/hostname
 
+# Network
 vim /etc/conf.d/net
-
 emerge --ask net-misc/dhcpcd
-
 rc-update add dhcpcd default
 rc-service dhcpcd start
-
 emerge --ask --noreplace net-misc/netifrc
-
 vim /etc/hosts
-
+# 127.0.0.1 localhost
+# ::1 localhost
+# 127.0.1.1 the
 emerge --ask sys-apps/pcmciautils
 
+# Su password
 passwd
 
+# Syslog
 emerge --ask app-admin/sysklogd
-
 rc-update add sysklogd default
 
+# Task 
 emerge --ask sys-process/cronie
-
 rc-update add cronie default
 
+# Fast grep
 emerge --ask sys-apps/mlocate
 
+# Ssh
 rc-update add sshd default
 
+# Time
 emerge --ask net-misc/chrony
-
 rc-update add chronyd default
 
+# Wireless 
 emerge --ask net-wireless/iw net-wireless/wpa_supplicant
 
+# Grub
 echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
-
 emerge --ask sys-boot/grub
-
 grub-install --target=x86_64-efi --efi-directory=/boot
-
 grub-mkconfig -o /boot/grub/grub.cfg
 
+# User
 useradd -m -G users,wheel,audio -s /bin/bash arctesian
 passwd arctesian
+emerge -aq app-admin/doas
+vim /etc/doas
+permit :wheel
+
+# GUI
 emerge --ask x11-base/xorg-server
-
 emerge --pretend --verbose x11-base/xorg-drivers
-
 eselect profile set default/linux/amd64/17.1/desktop/gnome
-
 emerge --ask gnome-base/gnome-light
-
 env-update && source /etc/profile
-
 rc-update add elogind boot
-
 rc-service elogind start
-
 emerge --ask --noreplace gui-libs/display-manager-init
-
 vim /etc/conf.d/display-manager
    DISPLAYMANAGER="gdm"
 
+# Display Manager
 rc-update add display-manager default
-
 emerge -aq xterm dwm st git alacritty
 
 exit
