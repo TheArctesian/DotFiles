822779ab0e68dadf0c89c438e4d89182671e232d
theArctesian <stephen.d.okita@gmail.com>
2022-06-20 02:15:59 +0000

    updates

--- distro/Arch/yay.sh
+++ distro/Arch/yay.sh
@@ -2,9 +2,8 @@ sudo pacman -S --needed base-devel
 # i think you can do this with doas now
 cd /opt
 sudo git clone https://aur.archlinux.org/yay-git.git
-sudo chown -R {name}:{name} yay-git
+sudo chown -R arctesian:arctesian yay-git
 cd yay-git
 makepkg -si
-yay
 
 
