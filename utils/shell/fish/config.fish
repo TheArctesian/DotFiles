123bb27477480267ef6a1b985195f64f4afb72bb
theArctesian <stephen.d.okita@gmail.com>
2022-07-08 18:08:34 -0700

    pee

--- utils/shell/fish/config.fish
+++ utils/shell/fish/config.fish
@@ -7,6 +7,7 @@ alias la "ls -a"
 alias lla "ls -la"
 alias lt  "ls --tree"
 alias vim "nvim"
-alias cls "clear"
+alias cls "clear" # this is the windows way of doing it that is easier
 alias p "ping -c 3 gentoo.org"
 alias clok "tty-clock -c -C 4 -S"
+alias py "python3" # this is the windows way of doing it that is easier
