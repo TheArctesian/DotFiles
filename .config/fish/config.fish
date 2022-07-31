b24e70c73eacc73919b31ac534bb396e87bfd091
theArctesian <stephen.d.okita@gmail.com>
2022-07-31 09:03:32 +0100

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,21 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-alias ls "lsd -l"
-alias la "ls -a"
-alias lla "ls -la"
-alias lt  "ls --tree"
+function fish_greeting
+	rxfetch
+	feh --bg-fill --randomize ~/Pictures/wallpaper/*
+end
+alias ls "lsd"
+alias la "lsd -la"
+alias lla "lsd -la"
+alias lt  "lsd --tree"
 alias vim "nvim"
-alias cls "clear" # this is the windows way of doing it that is easier
+alias cls "clear"
 alias p "ping -c 3 gentoo.org"
+alias py "python3"
 alias clok "tty-clock -c -C 4 -S"
-alias py "python3" # this is the windows way of doing it that is easier
+alias pull "git pull"
+alias neo "neovide"
+
+fish_add_path /home/arctesian/.spicetify
+
