51027bd528399f6bf12e162fe43d75b7aee471ec
theArctesian <stephen.d.okita@gmail.com>
2022-08-19 21:18:04 +0800

    fish

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -4,6 +4,10 @@ import dracula.draw
 config.load_autoconfig()
 
 c.url.start_pages = 'http://172.104.177.135:3333/'
+c.url.default_page =  'http://172.104.177.135:3333/'
+# behaviour [qi
+
+# = 'http://172.104.177.135:3333/'
 dracula.draw.blood(c, {
     'spacing': {
         'vertical': 6,
