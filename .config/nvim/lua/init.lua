ec5031e437041177a512c8d4e17f2ecf808c3613
theArctesian <stephen.d.okita@gmail.com>
2022-12-17 20:52:25 +0800

    hello again

---
+++ .config/nvim/lua/init.lua
@@ -0,0 +1,17 @@
+local api = vim.api
+local utils = require("utils")
+
+local core_conf_files = {
+  "globals.lua", -- some global settings
+  "options.vim", -- setting options in nvim
+  "autocommands.vim", -- various autocommands
+  "mappings.lua", -- all the user-defined mappings
+  "plugins.vim", -- all the plugins installed and their configurations
+  "colorschemes.lua", -- colorscheme settings
+}
+
+for _, name in ipairs(core_conf_files) do
+  local path = string.format("%s/core/%s", vim.fn.stdpath("config"), name)
+  local source_cmd = "source " .. path
+  vim.cmd(source_cmd)
+end
