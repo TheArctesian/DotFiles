8f10ec3b0dde08dd2fdcf9ba436b0f6ae878df14
theArctesian <stephen.d.okita@gmail.com>
2025-06-07 14:50:34 -0700

    update fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,10 @@ function fish_greeting
 	feh --bg-fill --randomize ~/Pictures/wallpaper/*
 end
 
+fish_add_path /home/okita/.cargo/bin/
+
+zoxide init fish | source
+
 alias vim "nvim"
 alias clok "tty-clock -c -C 4 -S"
 
@@ -16,7 +20,6 @@ alias p "ping -c 3 gentoo.org"
 alias py "python3"
 alias cls "clear"
 
-# alias cat "bat"
 
 # Git
 alias pull "git pull"
@@ -32,7 +35,18 @@ alias sec "cd ~/Documents/Second\ Brain/"
 alias uni "cd ~/Documents/Second\ Brain/Uni/"
 
 # ls 
-alias ls "lsd"
+# alias ls "lsd"
+alias ls "exa"
 fish_add_path /home/arctesian/.spicetify
 
 alias server="browser-sync start -s -f . --no-notify --host $LOCAL_UP --port 9000"
+
+
+# alias cat "bat"
+# alias curl "xh"
+# alias tmux "zellik"
+# gitui
+# alias du dust
+# dua disk analyser for dir
+# alias find fd
+# riprip
--- Deb/debian.sh
+++ Deb/debian.sh
@@ -1,15 +1,16 @@
-# chmod +x ubuntu.sh 
-
 # System update
 sudo apt update -y
 sudo apt upgrade -y 
 sudo apt install neofetch -y
+
 # Install packages
-
 sudo apt install curl -y
-sudo apt install file -y
 sudo apt install git -y
+sudo apt install nala -y
 
+# Terminal tools
+sudo apt install btop -y
+sudo apt install neovim -y
 
 # Development
 sudo apt install build-essential -y
@@ -33,11 +34,41 @@ sudo npm i -g yarn -y
 sudo apt install rustc -y
 sudo apt install cargo -y
 
-
-# fish 
+# Terminal enhancements
 sudo apt install fish -y
+sudo apt install alacritty -y
 sudo apt install fonts-firacode -y
 sudo apt install fonts-hack-ttf -y
 
-sudo apt-get upgrade && sudo apt-get upgrade
+# Applications - using apt where possible
+sudo apt install libreoffice -y
+sudo apt install code -y  # VSCode
 
+# Applications requiring external repositories
+# Brave browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update -y
+sudo apt install brave-browser -y
+
+# Obsidian - download and install deb file
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.3/obsidian_1.5.3_amd64.deb
+sudo apt install ./obsidian_1.5.3_amd64.deb -y
+rm obsidian_1.5.3_amd64.deb
+
+# Zotero - download and extract
+wget -q https://www.zotero.org/download/client/dl?channel=release&platform=linux-x86_64 -O zotero.tar.bz2
+sudo tar -xjf zotero.tar.bz2 -C /opt/
+sudo ln -s /opt/Zotero_linux-x86_64/zotero /usr/local/bin/zotero
+rm zotero.tar.bz2
+
+# Slack - using snap as an alternative
+sudo apt install snapd -y
+sudo snap install slack --classic
+
+# Final system update
+sudo apt update -y && sudo apt upgrade -y
+
+# Clean up
+sudo apt autoremove -y
+sudo apt autoclean -y
